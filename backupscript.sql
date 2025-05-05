33f275655a7f82943f2914c982246e5098f63c03
Ravi1983-dev <ravishankar.sg2@gmail.com>
2025-05-05 22:34:11 +0530

    Update backupscript.sql

--- backupscript.sql
+++ backupscript.sql
@@ -1,1 +1,1 @@
-EXEC msdb.dbo.rds_backup_database @source_db_name='Apple', @s3_arn_to_backup_to='arn:aws:s3:::clouds3basic/Apple3.bak',@overwrite_S3_backup_file=1;
+EXEC msdb.dbo.rds_backup_database @source_db_name=\'Apple\', @s3_arn_to_backup_to=\'arn:aws:s3:::clouds3basic/Apple3.bak\',@overwrite_S3_backup_file=1;
