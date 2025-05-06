92271a158b0bfcde8d1f0ce93bced8d2b43ec3fd
Ravi1983-dev <ravishankar.sg2@gmail.com>
2025-05-06 10:39:17 +0530

    Update psReadFile_Github.ps1

--- psReadFile_Github.ps1
+++ psReadFile_Github.ps1
@@ -3,5 +3,5 @@
 #$url = "https://github.com/Ravi1983-dev/devopsdemo/blob/e3f4b1f32ae3b7a7220a7b648ae81d31d612a382/rdsServers.txt"
 
 $content = Get-Content -Path "rdsServers.txt" -Raw
-Write-Host $($content)
+Write-Host $content
 
