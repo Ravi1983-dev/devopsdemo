c45d978198f882b099c74e42e224c7c9c31020e1
Ravi1983-dev <ravishankar.sg2@gmail.com>
2025-05-06 10:32:28 +0530

    Add files via upload

---
+++ psReadFile.ps1.ps1
@@ -0,0 +1,8 @@
+﻿# Raw file URL from GitHub
+#$url = "https://raw.githubusercontent.com/username/repo/branch/path/to/file.txt"
+#$url = "https://github.com/Ravi1983-dev/devopsdemo/blob/e3f4b1f32ae3b7a7220a7b648ae81d31d612a382/rdsServers.txt"
+
+$content = Get-Content -Path "rdsServers.txt" -Raw
+
+$response = Invoke-WebRequest -Uri $url -UseBasicParsing
+Write-Output $response.Content
